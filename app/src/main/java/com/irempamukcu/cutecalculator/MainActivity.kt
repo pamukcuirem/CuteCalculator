253c5dcb5760449a8db9bb5aee2e38d0be2bd3ec
pamukcuirem <irempamukcu1@gmail.com>
2024-04-08 23:48:42 +0300

    App with basic calculating and 5 themes.

--- app/src/main/java/com/irempamukcu/cutecalculator/MainActivity.kt
+++ app/src/main/java/com/irempamukcu/cutecalculator/MainActivity.kt
@@ -17,7 +17,6 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
     private lateinit var sharedPreferences: SharedPreferences
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -47,7 +46,7 @@ class MainActivity : AppCompatActivity() {
             binding.clear
         )
 
-        //process of all buttons
+
         for(button in buttons){
           button.setOnClickListener {
               val buttonText = button.text.toString()
